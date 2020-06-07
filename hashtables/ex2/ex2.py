b1953dca4c92e1986cdeb41b083a22c6764288f2
Patrick Roach <patrickevanroach@gmail.com>
2020-06-07 15:48:09 -0600

    mvp hit

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,108 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = ['']
+    next_source = None
+
+    for i in tickets:
+        if i.source not in cache:
+            cache[i.source] = i.destination
+
+    for i in cache:
+        if i == 'NONE':
+            next_source = cache[i]
+
+
+    while route[-1] != 'NONE':
+        if route[0] == '':
+            route[0] = next_source
+        else:
+            next_source = cache[next_source]
+            route.append(next_source)
+
+
 
     return route
+
+
+if __name__ == '__main__':
+    ticket_1 = Ticket("PIT", "ORD")
+    ticket_2 = Ticket("XNA", "SAP")
+    ticket_3 = Ticket("SFO", "BHM")
+    ticket_4 = Ticket("FLG", "XNA")
+    ticket_5 = Ticket("NONE", "LAX")
+    ticket_6 = Ticket("LAX", "SFO")
+    ticket_7 = Ticket("SAP", "SLC")
+    ticket_8 = Ticket("ORD", "NONE")
+    ticket_9 = Ticket("SLC", "PIT")
+    ticket_10 = Ticket("BHM", "FLG")
+
+    tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+                ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+    print(reconstruct_trip(tickets, 10))
+# # One-Way Flight Trip
+
+# You've booked a _really_ cheap one-way flight. Unfortunately, that means
+# you have _tons_ of layovers before you reach your destination. The
+# morning of, you woke up late and had to scramble to the airport to catch
+# your first flight. However, in your rush, you accidentally scrambled all
+# your flight tickets. You don't know the route of your entire trip now!
+
+# Write a function `reconstruct_trip` to reconstruct your trip from your
+# mass of flight tickets. Each ticket is represented as a class with the
+# following form:
+
+# ```
+# class Ticket:
+#     def __init__(self, source, destination):
+#         self.source = source
+#         self.destination = destination
+# ```
+
+# where the `source` string represents the starting airport and the
+# `destination` string represents the next airport along our trip. The
+# ticket for your first flight has a destination with a `source` of
+# `NONE`, and the ticket for your final flight has a `source` with a
+# `destination` of `NONE`. 
+
+# Pseudocode for an array of `Tickets` might look like this:
+
+# ```
+# tickets = [
+#     Ticket{ source: "PIT", destination: "ORD" },
+#     Ticket{ source: "XNA", destination: "CID" },
+#     Ticket{ source: "SFO", destination: "BHM" },
+#     Ticket{ source: "FLG", destination: "XNA" },
+#     Ticket{ source: "NONE", destination: "LAX" },
+#     Ticket{ source: "LAX", destination: "SFO" },
+#     Ticket{ source: "CID", destination: "SLC" },
+#     Ticket{ source: "ORD", destination: "NONE" },
+#     Ticket{ source: "SLC", destination: "PIT" },
+#     Ticket{ source: "BHM", destination: "FLG" }
+# ]
+# ```
+
+# Your function should output an array of strings with the entire route of
+# your trip. For the above example, it should look like this:
+
+# ```
+# ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+# ```
+
+# Your solution should run in linear time. You can assume that your
+# function will always be handed a valid ticket chain as input. 
+
+# ## Hints
+
+# * The crux of this problem requires us to 'link' tickets together to
+#   reconstruct the entire trip. For example, if we have a ticket `('SJC',
+#   'BOS')` that has us flying from San Jose to Boston, then there exists
+#   another ticket where Boston is the starting location, `('BOS',
+#   'JFK')`. 
+
+# * We can hash each ticket such that the starting location is the key and
+#   the destination is the value. Then, when constructing the entire
+#   route, the `i`th location in the route can be found by checking the
+#   hash table for the `i-1`th location.
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,19 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for i in arrays:
+        for j in i:
+            if j not in cache:
+                cache[j] = 1
+            else:
+                cache[j] += 1
+    
+    for key,v in cache.items():
+        if v > 1:
+            result.append(key)
 
     return result
 
@@ -15,3 +28,38 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+# # Intersections of Multiple Lists
+
+# Find the intersection between multiple lists of integers.
+
+# Do not use numpy or sets to solve this; use `dict` or hashtables,
+# please.
+
+# We're given a list of lists that contain integers:
+
+# ```python
+# [
+#     [1, 2, 3, 4, 5],
+#     [12, 7, 2, 9, 1],
+#     [99, 2, 7, 1,]
+# ]
+# ```
+
+# And we need to compute the _intersection_, that is, numbers that exist
+# in all lists.
+
+# From the above input, the return value will be:
+
+# ```
+# [1, 2]
+# ```
+
+# Because those are the numbers that exist in all the lists.
+
+# (Output can be in any order.)
+
+# Limits:
+
+# * There can be up to 10 lists in the list of lists.
+# * The lists can contain up to roughly 1,000,000 elements each.
